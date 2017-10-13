142c3c1dbed4105a9192d0d4c64fa14418fe3a2b
brians444 <brians444@gmail.com>
2017-10-13 09:37:14 -0300

    Agregando suma de control

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -111,7 +111,7 @@ void MainWindow::onNewDataArrived(QStringList newData)
     if(plotting)
     {
         int dataListSize = newData.size();
-        uint16_t suma = 0, suma_recv=0;
+        long suma = 0, suma_recv=0;
         qDebug()<<"Lista = "<<newData;
         suma_recv = newData[dataListSize-1].toLong();
         for(int i = 0; i < dataListSize-1; i++)
